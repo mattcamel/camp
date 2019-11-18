622ca29ff8c16216b6644758d3b087a89f9c1c25
Matt Camellato <mattcamellato@gmail.com>
2019-11-18 23:26:10 +0000

    app.js port update

--- app.js
+++ app.js
@@ -62,8 +62,8 @@ app.use('/', indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-// app.listen(process.env.PORT, process.env.IP);
+app.listen(process.env.PORT, process.env.IP);
 
-app.listen(3000, () => {
-	console.log("Yelp Camp server listening");
-});
+// app.listen(3000, () => {
+// 	console.log("Yelp Camp server listening");
+// });
